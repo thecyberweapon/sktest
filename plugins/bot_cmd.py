9c14b30b19dd2a6c82545de27defc0069e48112f
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2021-10-26 01:03:33 +0530

    Update bot_cmd.py

--- plugins/bot_cmd.py
+++ plugins/bot_cmd.py
@@ -6,16 +6,16 @@ from pyrogram import filters
 from main import validate,backgen
 chatid = os.environ.get("FORWARD_ID")
     
-goat = False
+#goat = False
 
-
+'''
 @Bot.on_message(filters.private)
 async def backo(c,m):
    if not goat:
       asyncio.get_event_loop().create_task(backgen(c))
       goat=True
    await m.continue_propagation()
-
+'''
 @Bot.on_message(filters.private & filters.command(["start","help"]))
 async def _start_cmd(bot,vishal):
     await vishal.reply("Hehe I can do many things that u can't do manually")
