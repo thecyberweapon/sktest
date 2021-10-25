8e194c6f7529d5edd859cab42012096670f85117
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2021-10-25 16:42:42 +0530

    Update start_cmd.py

--- plugins/start_cmd.py
+++ plugins/start_cmd.py
@@ -1,1 +1,17 @@
+import logging
+import pyrogram
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import Client as Bot
+from pyrogram import filters
 
+
+@Bot.on_message(filters.private & filters.command(["start","help"]))
+async def _start_cmd(bot,vishal):
+    await vishal.reply("Hehe I can do many things that u can't do manually")
+
+@Bot.on_message(filters.private & filters.command(["log"]))
+async def _log_cmd(bot,vishal):
+   try:
+      await vishal.reply_document("logs/Logs.txt")
+   except Exception as e:
+      await vishal.reply(f"Error\n{e}")
