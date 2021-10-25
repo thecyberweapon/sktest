dc89ae9a7a6d3fd17953f3c9255dc04284d1aea7
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2021-10-25 21:57:25 +0530

    Update sendmsg.py

--- plugins/sendmsg.py
+++ plugins/sendmsg.py
@@ -1,7 +1,7 @@
 import logging,os, requests
 import pyrogram
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram import Client as Bot
+from main import app
 from pyrogram import filters
 
 chatid = os.environ.get("FORWARD_ID")
@@ -9,6 +9,6 @@ bot_token = os.environ.get("BOT_TOKEN")
 
 
 async def sendmsg(text):
-     #await Bot.send_message(chat_id=chatid,text=text)
-     requests.get(url=f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chatid}&text={text}")
+     await app.send_message(chat_id=chatid,text=text)
+     #requests.get(url=f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chatid}&text={text}")
     
